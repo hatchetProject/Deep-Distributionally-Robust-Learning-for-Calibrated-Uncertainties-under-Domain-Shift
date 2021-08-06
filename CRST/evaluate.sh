928fd266c4cb3cd01164ca1ceecdc5883ce8c63c
Haoxuan Wang <35368504+hatchetProject@users.noreply.github.com>
2021-08-06 16:57:17 +0800

    Delete CRST directory

--- CRST/evaluate.sh
+++
@@ -1,1 +0,0 @@
-CUDA_VISIBLE_DEVICES=2 python evaluate.py --data-dir dataset/cityscapes/
--- CRST/occlusion_optical_flow/demo.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export PYTHONPATH=RAFT:RAFT/core
-
-CUDA_VISIBLE_DEVICES=0 python occlusion_detection.py --model=RAFT/models/raft-sintel.pth
